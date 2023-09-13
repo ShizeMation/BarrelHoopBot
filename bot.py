4a5e5b7b0766e11ee07ee04e397a09ce75532c29
ShizeMation <1732145@gmail.com>
2023-09-13 01:15:48 -0400

    Slight improvement

--- bot.py
+++ bot.py
@@ -24,15 +24,17 @@ MIDDLE_DIM = {"top": 629, "left": 744, "width": 27, "height": 27}
 MATCH_METHOD = cv.TM_SQDIFF_NORMED
 
 runProgram = True
-runBot = True
+runBot = False
+state = "waiting"
 
 def stop():
     global runProgram
     runProgram = False
 
 def toggle():
-    global runBot
+    global runBot, state
     runBot = not runBot
+    state = "waiting"
 
 listener = kb.GlobalHotKeys({
     STOP_KEY: stop,
@@ -40,7 +42,6 @@ listener = kb.GlobalHotKeys({
 })
 listener.start()
 
-state = "waiting"
 delay = 0
 
 right_dim = {"top": 608, "left": 0, "width": 19, "height": 19}
@@ -61,20 +62,35 @@ with mss() as sct:
                     resMiddle = cv.matchTemplate(middleSS, MIDDLE_TEMPLATE, MATCH_METHOD)
                     if (cv.minMaxLoc(resMiddle)[0] < 0.05):
                         delay = time() - delay
-                        print(f"Finished timing: {delay}")
-                        if (delay < 0.5):
-                            right_dim["left"] = 1155
-                        elif (delay < 0.6):
-                            right_dim["left"] = 1200
+                        print(f"  Finished timing: {delay:.3f}")
+                        if (delay < 0.45):
+                            right_dim["left"] = 1120
+                        elif (delay < 0.48):
+                            right_dim["left"] = 1150
+                        elif (delay < 0.51):
+                            right_dim["left"] = 1190
+                        elif (delay < 0.54):
+                            right_dim["left"] = 1220
+                        elif (delay < 0.56):
+                            right_dim["left"] = 1260
+                        elif (delay < 0.58):
+                            right_dim["left"] = 1280
                         elif (delay < 0.65):
-                            right_dim["left"] = 1315
+                            right_dim["left"] = 1300
+                        elif (delay < 0.7):
+                            right_dim["left"] = 1340
+                        elif (delay < 0.73):
+                            right_dim["left"] = 1360
+                        elif (delay < 1):
+                            right_dim["left"] = 1380
                         else:
-                            right_dim["left"] = 1320
+                            state = "waiting"
+                            continue
                         state = "kicking"
                 case "kicking":
                     rightSS = cv.cvtColor(np.array(sct.grab(right_dim)), cv.COLOR_BGRA2GRAY)
                     resRight = cv.matchTemplate(rightSS, RIGHT_TEMPLATE, MATCH_METHOD)
                     if (cv.minMaxLoc(resRight)[0] < 0.2):
                         di.press(KICK_KEY)
-                        print("Kicked")
+                        print(f"    Kicked on: {right_dim['left']}")
                         state = "waiting"
